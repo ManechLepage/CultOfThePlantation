77b672746eb7eb5503ba3ca7a6903d8271f4e4cc
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-20 01:20:35 -0400

    PPL

--- tuskclimb/Scripts/building_ui.gd
+++ tuskclimb/Scripts/building_ui.gd
@@ -9,3 +9,5 @@ func _ready() -> void:
 		var building_instance = building_slot.instantiate()
 		
 		get_child(0).add_child(building_instance)
+		building_instance.get_child(0).texture = building.texture
+		building_instance.get_child(1).text = building.name
