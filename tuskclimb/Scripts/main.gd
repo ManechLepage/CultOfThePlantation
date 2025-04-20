bcb5ead272f483a13a29c1f49ebbb89c11f42357
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-20 11:34:44 -0400

    Jiste

--- tuskclimb/Scripts/main.gd
+++ tuskclimb/Scripts/main.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@onready var grid_map: GridManager = $GridMap
+
+var energy: int
+var food: int
+
+func _on_next_turn_button_pressed() -> void:
+	load_next_turn()
+
+func load_next_turn() -> void:
+	for building in grid_map.buildings:
+		energy += building.energy_production - building.energy_cost
+		food += building.food_production - building.food_cost
