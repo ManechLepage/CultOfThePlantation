548d990dabca3b9ebd51c79633c7f6d064f53a92
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-20 10:58:48 -0400

    Mjjot

--- tuskclimb/Scripts/grid_map.gd
+++ tuskclimb/Scripts/grid_map.gd
@@ -5,6 +5,8 @@ extends Node2D
 @onready var structures: TileMapLayer = $Structures
 @onready var structure_hover: TileMapLayer = $StructureHover
 
+@export var grass_tile: Building
+
 var buildings: Array[Building]
 
 var hovered: Building
@@ -25,7 +27,20 @@ func update_hover():
 		structure_hover.erase_cell(cell)
 	
 	if hovered:
-		structure_hover.set_cell(structure_hover.local_to_map(get_global_mouse_position()), 0, hovered.get_sprite())
+		var pos: Vector2i = structure_hover.local_to_map(get_global_mouse_position())
+		if hovered == grass_tile:
+			pos = adjust_pos(pos)
+		structure_hover.set_cell(pos, 0, hovered.get_sprite())
+
+func handle_placing() -> void:
+	if not hovered:
+		return
+	var pos: Vector2i = structures.local_to_map(get_global_mouse_position())
+	if hovered == grass_tile:
+		pos = adjust_pos(pos)
+		ground.set_cell(pos, 0, grass_tile.get_sprite())
+	else:
+		place_building(hovered, pos)
 
 func place_building(placed_building: Building, pos: Vector2i) -> void:
 	for building in buildings:
@@ -42,4 +57,7 @@ func place_building(placed_building: Building, pos: Vector2i) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Click"):
-		place_building(hovered, structures.local_to_map(get_global_mouse_position()))
+		handle_placing()
+
+func adjust_pos(pos: Vector2i) -> Vector2i:
+	return pos - Vector2i(0, 2)
